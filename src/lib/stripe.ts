d6a30f0eca4ecc0615d4477425e9e37b1d876bc2
wenzel audrin barboza <wenzelbarboza@gmail.com>
2025-04-02 12:03:13 +0300

    code cleanup 0.1

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -2,6 +2,6 @@ import "server-only";
 
 import Stripe from "stripe";
 
-console.log("this is the api key: ", process.env.STRIPE_SECRET_KEY);
+// console.log("this is the api key: ", process.env.STRIPE_SECRET_KEY);
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
